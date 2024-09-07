1608048873433129850bd718754f1a0882840be6
Terezie Mazáčová <terezkamazacova@post.cz>
2024-09-07 23:57:16 +0200

    nastylovani cele sekce header a pridani fontu, barev

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      playfair: ['Playfair Display', 'serif'],
+      outfit: ['Outfit', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'primary-600' : '#996830',
+
+        'neutral-900' : '#2C2C2C',
+        'neutral-700' : '#444444',
+      },
+      maxWidth: {
+        '1440' : '1440px',
+      },
+
+      padding: {
+        '112' : '112px'
+      },
+    },
   },
   plugins: [],
 }
